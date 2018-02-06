c01fe93015b8097a33849ebed5ebd9c9e719a2e6
GuttermanA <alexanderf.gutterman@gmail.com>
2018-02-06 11:48:57 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    self.artist.name
+  end
+
+  def title_and_artist
+    "#{self.title} - self.artist_name"
+  end
 end
